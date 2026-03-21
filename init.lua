12205d1e86fbd83e7b72db2ef403cf3cdb4c1a71
romeuriffatti2 <romeu.riffatti.2@gmail.com>
2026-03-21 01:28:54 -0300

    configs ip local romeu

--- init.lua
+++ init.lua
@@ -14,8 +14,10 @@ Services = {
 }
 
 -- Servers accept http login url, websocket login url or ip:port:version
+local serverIp = os.getenv("OTCLIENT_IP") or "26.226.119.223"
+g_logger.info(">> Login IP configured in init.lua: " .. serverIp)
 Servers = {
-    LocalServer = "26.226.119.223:7171:1098"
+    LocalServer = serverIp .. ":7171:1098"
 }
 
 --Server = "ws://otclient.ovh:3000/"
